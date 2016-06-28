63ae9cc931e119871066e3d4e548c0ce21814123
John Jenson <JRRJenson@gmail.com>
2016-06-28 15:31:03 -0700

    Complete initial integration of WYSIWYG selection interface

--- client/components/StartTest.js
+++ client/components/StartTest.js
@@ -1,6 +1,8 @@
 // This component should later be removed
 // or completely integrated into dashboard
 
+// the hidden form values should be passed in as props
+
 import React, { Component } from 'react';
 
 class StartTest extends Component {
@@ -10,7 +12,10 @@ class StartTest extends Component {
       <div>
         <h3>Add new test</h3>
         <form className="sourcePath" action="/selection/addtest" method="post">
-          <input name="url" type="text" value="http://hackreactor.com" />
+          <label>Test Name<input name="name" type="text" /></label><br />
+          <label>Page A URL<input name="url" type="text" /><br /></label>
+          <input name="ab" type="hidden" value="a" />
+          <input name="page_id" type="hidden" value="2" />
           <input type="submit" />
         </form>
       </div>
--- server/api/selection/routes.js
+++ server/api/selection/routes.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const path = require('path');
 
 // styles for our text overlay and selection highlighting
+const htmlFile = fs.readFileSync(path.resolve(__dirname, './assets/modals.html')).toString();
+const htmlTag = `<div>${htmlFile}</div>`;
+
+// styles for our text overlay and selection highlighting
 const styleFile = fs.readFileSync(path.resolve(__dirname, './assets/styles.css')).toString();
 const styleTag = `<style>${styleFile}</style>`;
 
@@ -20,17 +24,22 @@ module.exports = (app) => {
       ['http://code.jquery.com/jquery.js'],
       (err, window) => {
         const $ = window.jQuery;
-        $('body').append('<h1 class="openab-header">Select links or buttons to test</h1>');
-        $('body').append('<h1 class="openab-header openab-domTree"></h1>');
-        $('body').append('<div class="openab-modal openab-nav-a openab-select"><form class="openab-b-url" action="/selection/addTest" method="post"><label>URL for Version B<input name="url" type="url"/></label><input type="submit"/></form></div>');
+        $('body').append(htmlTag);
         $('head').append(styleTag);
         // pass information that our POST request will need, through the window object
         // is this okay to do?  are there situations where this would fail?
         $('head').append(`<script>window.openab = {
           ab: '${req.body.ab}',
-          test_id: ${req.body.test_id},
+          name: '${req.body.name}',
+          page_id: ${req.body.page_id},
           url: '${req.body.url}'
         } </script>`);
+        if (req.body.ab === 'b') {
+          $('head').append(`<script>
+          window.openab.url_a = '${req.body.url_a}';
+          window.openab.dom_a = '${req.body.dom_a}';
+          </script>`);
+        }
         $('body').append(scriptTag);
         res.send(window.document.documentElement.innerHTML);
       }
