959de3ffb2b6ee1b1c0b65b8f825625d0f5ea68b
John Jenson <JRRJenson@gmail.com>
2016-06-29 15:47:41 -0700

    Consolidate testing for TravisCI test
    Add postgres to travis
    
    Add seed-db to travis

--- server/api/selection/routes.js
+++ server/api/selection/routes.js
@@ -18,7 +18,13 @@ const scriptTag = `<script>${scriptFile}</script>`;
 module.exports = (app) => {
   app.post('/selection/addTest', (req, res, next) => {
     // on newTest initiated from dashboard
-    console.log(req.body);
+    const ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+    console.log(req.body, req.headers['x-forwarded-for'],
+     req.connection.remoteAddress,
+     req.socket.remoteAddress);
     jsdom.env(
       req.body.url,
       ['http://code.jquery.com/jquery.js'],
