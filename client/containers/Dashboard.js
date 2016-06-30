86827b193ea337d60800d221b3c649b28363e9f0
Jen J Wong <jenjwong@gmail.com>
2016-06-29 21:30:46 -0700

    apply stash after rebase

--- client/containers/Dashboard.js
+++ client/containers/Dashboard.js
@@ -3,10 +3,12 @@ import TestResults from './TestResults';
 import StartTest from '../components/StartTest';
 import ClickMap from '../components/ClicksMap';
 import LineChartData from './LineChartData';
+import NavBar from '../components/NavBar';
 
 function Dashboard() {
   return (
     <div>
+      <NavBar />
       <StartTest />
       <ClickMap />
       <LineChartData />
